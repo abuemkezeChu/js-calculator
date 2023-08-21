7af3b6b43c806a0c4014a00ddd341b43bf22be98
Abuemkeze Chu <abuemkeze@gmail.com>
2023-08-21 16:57:54 +0100

    feat: add functionality and handle click events

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,1 +1,20 @@
 'use strict'
+
+let display = document.getElementById('display')
+
+// function to clear screen
+const clearScreen = () => {
+  display.value = ''
+}
+
+// display values 
+const displayScreen = (value) => {
+  display.value += value
+}
+
+// calculate
+const calculate = () => {
+  let calc = display.value
+  let result = eval(calc)
+  display.value = result
+}
