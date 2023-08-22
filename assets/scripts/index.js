26534ead61e1b57a849cb943a170cfdd8e927184
Abuemkeze Chu <abuemkeze@gmail.com>
2023-08-22 14:48:46 +0100

    feat: add click event listener to buttons

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,20 +1,51 @@
 'use strict'
 
-let display = document.getElementById('display')
+const display = document.getElementById('display')
+const buttons = document.querySelectorAll('button')
 
 // function to clear screen
 const clearScreen = () => {
   display.value = ''
 }
 
-// display values 
+// display values
 const displayScreen = (value) => {
   display.value += value
 }
 
 // calculate
 const calculate = () => {
-  let calc = display.value
-  let result = eval(calc)
+  const calc = display.value
+  const result = eval(calc)
   display.value = result
 }
+
+//  backspace
+const backspace = () => {
+  let output = display.value
+  output = String(output).slice(0, -1)
+  return output
+}
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    switch (button.value) {
+      case 'AC':
+        clearScreen()
+        break
+
+      case '=':
+        calculate()
+        break
+
+      case 'DEL':
+        const erase = backspace()
+        display.value = erase
+        break
+
+      default:
+        displayScreen(button.value)
+        break
+    }
+  })
+})
